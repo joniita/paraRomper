4d044b5b723c99c76be5bd0522c77fda230fa4b0
joniita <joniita.d@gmail.com>
2023-08-09 21:14:26 -0300

    Agrego funcion para gestionar los datos recividos e imprimir en consola su nombre y precio

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
 const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
+function showData(dataArray) {
+    dataArray.map(i => {
+        //console.log(i);
+        console.log(`El auto es un ${i.name} y vale: ${i.currency} ${i.cost}`);
+    })
+}
+
 fetch(URL)
     .then(response => response.json())
-    .then(data => console.log(data.products));
+    .then(data => showData(data.products));
 
