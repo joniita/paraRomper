c6cff9fd120fb8007c9042ae0e5664b6371adfbf
joniita <joniita.d@gmail.com>
2023-08-10 23:12:33 -0300

    creo un indice en el Local Storage para guardar un dato en booleano

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-
+// Consulto si el LocalStoraje esta disponible en el navegador
 if (typeof(Storage) !== "undefined") {
     console.log("Storage disponible");
 } else {
     console.log("Storage no disponible");
 }
 
+// Creo una variable para guardar el valor de la sesion en booleano
+const sesion = {login: true};
 
+// guardo el valor de la sesion en el Local Storage convirtiendolo en texto antes
+localStorage.setItem("sesionIniciada", JSON.stringify(sesion))
+
+
+
+
+
+
+
+
+// Fetch
 const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
 function showData(dataArray) {
